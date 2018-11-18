2e16d06647e401b63a09a5dd3f2c9510e2221984
weichungw <weichunw@usc.edu>
2018-11-18 15:03:48 -0800

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgxMapboxGLModule } from 'ngx-mapbox-gl';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +9,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxMapboxGLModule.withConfig({
+      accessToken: "pk.eyJ1Ijoid2VpY2h1bmd3IiwiYSI6ImNqbWdzdDM1Zzc5Z3AzcG9nYjBmNGdzN3EifQ.n3Rc4TxBtrgK1zCX5dkozg"
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
